87f8fa5c4c3ccefd44c29dc7e55115f5ac2db565
pupil <pupil@ivkhk.ee>
2019-09-10 13:35:27 +0300

    Задали null для id

--- src/classes/App.java
+++ src/classes/App.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package classes;
 
 import entity.Book;
@@ -10,14 +6,10 @@ import entity.History;
 import entity.Reader;
 import java.util.Date;
 
-/**
- *
- * @author melnikov
- */
 public class App {
     public void run(){
         Book book = new Book();
-        book.setId(1L);
+        book.setId(null);
         book.setTitle("Voina i mir");
         book.setAuthor("L.Tolstoy");
         book.setYear(2010);
@@ -28,18 +20,18 @@ public class App {
         System.out.println("Вывод метода toString(): ");
         System.out.println(book.toString());
         
-        Book book2 = new Book(2L, "Otsi i deti", "I.Turgenev", 2011);
+        Book book2 = new Book(null, "Otsi i deti", "I.Turgenev", 2011);
         System.out.println(book2.toString());
         
         book.setYear(2012);
         System.out.println(book.toString());
         
-        Reader reader = new Reader(1L, "Ivan", "Ivanov", 10, 10, 2000);
+        Reader reader = new Reader(null, "Ivan", "Ivanov", 10, 10, 2000);
         System.out.println(reader.toString());
                 
         System.out.println("Выдача книги:");
         History history = new History();
-        history.setId(1L);
+        history.setId(null);
         history.setBook(book);
         history.setReader(reader);
         history.setTakeOnDate(new Date());
